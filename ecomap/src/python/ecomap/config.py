0b4a15a305bafb652b1872163756d76fd76c2fb5
= <frut.jass779@gmail.com>
2015-10-13 15:59:47 +0300

    added config.py

---
+++ ecomap/src/python/ecomap/config.py
@@ -0,0 +1,31 @@
+import ConfigParser
+import time
+
+
+configFilePath = '../../../etc/ecomap.conf'
+
+
+def parseConfs():
+    config = ConfigParser.RawConfigParser()
+    config.readfp(open(configFilePath))
+    return {key: value for (key, value) in config.items('ecomap')}
+
+
+class Config(object):
+    obj = None
+    timeDelta = 60 * 15  # 60 secs * 15 minutes
+    _deathTime = None
+
+    def __new__(cls):
+        if cls.obj is None:
+            cls.obj = object.__new__(cls)
+            cls._deathTime = time.time() + cls.timeDelta
+            cls.obj.conf = parseConfs()
+        if cls._deathTime < time.time():
+            cls.obj = object.__new__(cls)
+            cls._deathTime = time.time()
+            cls.obj.conf = parseConfs()
+        return cls.obj
+
+if __name__ == '__main__':
+    x = Config()

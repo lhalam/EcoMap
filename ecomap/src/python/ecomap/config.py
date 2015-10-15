8070d185dc6c80f1c0a0095e01fc4e25f4559cc4
= <frut.jass779@gmail.com>
2015-10-15 11:45:00 +0300

    edited _parseConfs method

--- ecomap/src/python/ecomap/config.py
+++ ecomap/src/python/ecomap/config.py
@@ -10,12 +10,12 @@ class Config(object):
 
     def __init__(self):
         self.lifeTime = time() + 900            # current time + 15 minutes
-        logger.debug('Created instance with lifeTime: {}'.
+        logger.debug('Inited instance with lifeTime: {}'.
                      format(self.lifeTime))
 
     def __new__(cls):
         if not hasattr(cls, '_instance') or cls._instance.lifeTime < time():
-            logger.debug('Create instance if it not exists or lifeTime expired')
+            logger.debug('Create instance if it not exists or lifeTime ended')
             cls._instance = object.__new__(cls)
             cls._parseConfs(cls._instance)
         logger.debug('Returned instance: {}'.format(cls._instance))
@@ -25,20 +25,27 @@ class Config(object):
         logger.debug('Parsed ecomap.conf')
         config = SafeConfigParser()             # create config object
         config.readfp(open(configFilePath))     # read file
-        sections = config.sections()            # get all sections
         self.config = {}
-        for section in sections:                # for each section
-            for (key, value) in config.items(section):  # for each key/value
-                if key != 'password':           # if key == password skip
+        for section in config.sections():                # for each section
+            for (key, value) in config.items(section):   # for each key/value
+                if value and key != 'password':
                     try:
-                        try:
-                            value = int(value)  # try to convert value into int
-                        except ValueError:
-                            value = float(value)
+                        value = config.getboolean(section, key)
                     except ValueError:
-                        pass
+                        try:
+                            if '.' in value:
+                                value = config.getfloat(section, key)
+                            else:
+                                value = config.getint(section, key)
+                        except ValueError:
+                            value = unicode(config.get(section, key).
+                                            decode('utf-8'))
                 self.config[section + '.' + key] = value
 
 if __name__ == '__main__':
     configFilePath = '../../../etc/ecomap.conf'
     x = Config()
+    print x.config
+    print '=' * 80
+    y = Config()
+    print y.config

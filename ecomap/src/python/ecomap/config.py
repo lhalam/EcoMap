b183aa138c585e27edb606d94218e49de3b9b3a9
= <frut.jass779@gmail.com>
2015-10-16 20:47:14 +0300

    added docstrings and pylint log

--- ecomap/src/python/ecomap/config.py
+++ ecomap/src/python/ecomap/config.py
@@ -1,15 +1,23 @@
+"""
+Module which contains Config class. This class is singleton.
+It exists to parse *.conf files and return dictionary,
+which contains configuration from those files. Every 15 minutes
+it returns new dictionary which contains updated configs.
+"""
 from ConfigParser import SafeConfigParser
 import logging
 import time
 import unittest
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 REFRESH_TIME = 900                               # 15 minutes
 PASSWORD = 'password'
 
 
 class Singleton(type):
-
+    """
+    Metaclass for make Config class singleton.
+    """
     def __call__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
             cls._instance = super(
@@ -18,6 +26,12 @@ class Singleton(type):
 
 
 class Config(object):
+
+    """
+    Singleton class which returns object which have method
+    to parse config file every 15 minutes.
+    :param path: name of file
+    """
     __metaclass__ = Singleton
 
     def __init__(self, path):
@@ -28,6 +42,12 @@ class Config(object):
         self.logger.debug('Initialized instance at: %s', time.time())
 
     def get_config(self):                        # method which checks if we
+        """
+        Checks if it is needed to reload configs.
+        Before calling parsing functian checks if
+        elapsed 15 minutes after last update.
+        returns: dictionary
+        """
         self.logger.debug('Check if need to update at %s', time.time())
         if self.update_time < time.time():       # need to update configs
             self.config = {}                     # nullify configs dictionary
@@ -36,6 +56,9 @@ class Config(object):
         return self.config
 
     def _parse_confs(self):
+        """
+        Parses config file and returns dictionary.
+        """
         self.logger.debug('Parsed ecomap.conf at %s', (time.time()))
         config = SafeConfigParser()              # create config object
         config.readfp(open(self.path))           # read file
@@ -51,21 +74,125 @@ class Config(object):
 
 
 if __name__ == '__main__':
-    class Test(unittest.TestCase):
 
+    FIRST = Config('../../../etc/ecomap.conf')
+    SECOND = Config('../../../etc/ecomap.conf')
+
+    class Test(unittest.TestCase):
+        """
+        Test class which inherits unittest.TestCase and
+        provides 3 tests for Config class.
+        """
         def test_sameinstances(self):
-            self.a = Config('../../../etc/ecomap.conf')
-            self.b = Config('../../../etc/ecomap.conf')
-            self.assertEquals(self.a, self.b)
+            """
+            Check if different calls returns same instance.
+            """
+            self.assertEquals(FIRST, SECOND)
 
         def test_config(self):
-            self.a = Config('../../../etc/ecomap.conf')
-            self.b = Config('../../../etc/ecomap.conf')
-            self.assertEquals(self.a.get_config(), self.b.get_config())
+            """
+            Check if different calls return same configs.
+            """
+            self.assertEquals(FIRST.get_config(), SECOND.get_config())
 
         def test_updatetime(self):
-            self.a = Config('../../../etc/ecomap.conf')
-            self.b = Config('../../../etc/ecomap.conf')
-            self.assertEquals(self.a.update_time, self.b.update_time)
+            """
+            Check if different calls have same update_time
+            """
+            self.assertEquals(FIRST.update_time, SECOND.update_time)
 
     unittest.main()
+
+# ************* Module config
+# W: 23,12: Attribute '_instance' defined outside __init__ (attribute-defined-outside-init)
+# R: 28, 0: Too few public methods (1/2) (too-few-public-methods)
+# R: 79, 4: Too many public methods (48/20) (too-many-public-methods)
+
+
+# Report
+# ======
+# 52 statements analysed.
+
+# Statistics by type
+# ------------------
+
+# +---------+-------+-----------+-----------+------------+---------+
+# |type     |number |old number |difference |%documented |%badname |
+# +=========+=======+===========+===========+============+=========+
+# |module   |1      |1          |=          |100.00      |0.00     |
+# +---------+-------+-----------+-----------+------------+---------+
+# |class    |3      |3          |=          |100.00      |0.00     |
+# +---------+-------+-----------+-----------+------------+---------+
+# |method   |7      |7          |=          |100.00      |0.00     |
+# +---------+-------+-----------+-----------+------------+---------+
+# |function |0      |0          |=          |0           |0        |
+# +---------+-------+-----------+-----------+------------+---------+
+
+
+
+# Raw metrics
+# -----------
+
+# +----------+-------+------+---------+-----------+
+# |type      |number |%     |previous |difference |
+# +==========+=======+======+=========+===========+
+# |code      |52     |52.53 |52       |=          |
+# +----------+-------+------+---------+-----------+
+# |docstring |34     |34.34 |34       |=          |
+# +----------+-------+------+---------+-----------+
+# |comment   |0      |0.00  |0        |=          |
+# +----------+-------+------+---------+-----------+
+# |empty     |13     |13.13 |13       |=          |
+# +----------+-------+------+---------+-----------+
+
+
+
+# Messages by category
+# --------------------
+
+# +-----------+-------+---------+-----------+
+# |type       |number |previous |difference |
+# +===========+=======+=========+===========+
+# |convention |0      |0        |=          |
+# +-----------+-------+---------+-----------+
+# |refactor   |2      |2        |=          |
+# +-----------+-------+---------+-----------+
+# |warning    |1      |1        |=          |
+# +-----------+-------+---------+-----------+
+# |error      |0      |0        |=          |
+# +-----------+-------+---------+-----------+
+
+
+
+# Messages
+# --------
+
+# +-------------------------------+------------+
+# |message id                     |occurrences |
+# +===============================+============+
+# |too-many-public-methods        |1           |
+# +-------------------------------+------------+
+# |too-few-public-methods         |1           |
+# +-------------------------------+------------+
+# |attribute-defined-outside-init |1           |
+# +-------------------------------+------------+
+
+
+
+# Global evaluation
+# -----------------
+# Your code has been rated at 9.42/10 (previous run: 9.42/10, +0.00)
+
+# Duplication
+# -----------
+
+# +-------------------------+------+---------+-----------+
+# |                         |now   |previous |difference |
+# +=========================+======+=========+===========+
+# |nb duplicated lines      |0     |0        |=          |
+# +-------------------------+------+---------+-----------+
+# |percent duplicated lines |0.000 |0.000    |=          |
+# +-------------------------+------+---------+-----------+
+
+
+

3e20eae6f78ef4abb4dd18f7e14f3cd19d8bc0d6
Vadim Padalko <vadime.padalko@gmail.com>
2015-10-26 23:25:03 +0200

    added required modules

---
+++ ecomap/src/python/ecomap/config.py
@@ -0,0 +1,64 @@
+"""
+Module which contains Config class. This class is singleton.
+It exists to parse *.conf files and return dictionary,
+which contains configuration from those files. Every 15 minutes
+it returns new dictionary which contains updated configs.
+"""
+import logging
+import os
+import time
+
+from ConfigParser import SafeConfigParser
+
+from utils import get_logger
+from utils import Singleton
+
+REFRESH_TIME = 900
+PASSWORD = 'password'
+CONFIG_PATH = os.path.join(os.environ['CONFROOT'], 'ecomap.conf')
+get_logger()
+
+
+class Config(object):
+    """
+    Singleton class to get configs
+    """
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        self.config = {}
+        self.update_time = 0
+        self.path = CONFIG_PATH
+        self.log = logging.getLogger('config_parser')
+        self.log.info('Create instance of Config parser.')
+
+    def get_config(self):
+        """
+        Call parse method if it needed.
+        Returns:
+            dictionary, containing configs
+        """
+        if self.update_time + REFRESH_TIME < time.time():
+            self.log.info('Refresh configs')
+            self.update_time = time.time()
+            self._parse_confs()
+        return self.config
+
+    def _parse_confs(self):
+        """
+        Parses config file.
+        """
+        self.log.info('Parse ecomap.conf.')
+        config = SafeConfigParser()
+        config.readfp(open(self.path))
+        sections = config.sections()
+        temp_config = {}
+        for section in sections:
+            for (key, value) in config.items(section):
+                if value and key != PASSWORD:
+                    try:
+                        value = eval(value)
+                    except NameError:
+                        pass
+                temp_config[section + '.' + key] = value
+        self.config = temp_config

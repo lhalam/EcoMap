802391d67e6510dab829ee849c6bb73f03b160c7
Vadim Padalko <vadime.padalko@gmail.com>
2015-10-26 20:20:03 +0200

    edited docstrings

--- ecomap/src/python/utils.py
+++ ecomap/src/python/utils.py
@@ -6,19 +6,24 @@ CONF_PATH = os.path.join(os.environ['CONFROOT'], 'log.conf')
 
 
 def get_logger():
+    """function for configuring default logger object
+    from standard logging library
 
-    """create a regular logger as usual.
-        :return:  configured logger object.
-        usage: import this method to your
-        module and call it.
+        Returns:
+            configured logger object.
+
+        Usage:
+            import this method to your
+            module and call it.
+            then define a new logger object as usual
+
     """
-
     return config.fileConfig(CONF_PATH)
 
 
 class Singleton(type):
     """
-    using a singleton pattern to work with only one possible instance of Pool
+    using a Singleton pattern to work with only one possible instance of Pool
     """
     def __call__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):

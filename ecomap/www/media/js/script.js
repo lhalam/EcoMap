c7c0e12a8e6b24a8f934e0be355881bf9fa3636c
Vlad <g.vlad96>
2015-11-04 13:26:12 +0200

    Update front end

--- ecomap/www/media/js/script.js
+++ ecomap/www/media/js/script.js
@@ -1,131 +1,27 @@
-var app=angular.module('app',['ui.bootstrap'])
-app.controller('DatepickerDemoCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+  var toogle=document.getElementById("toggle");
+  var filter=document.getElementById("filter");
+  console.log(toogle)
+  toogle.addEventListener("click" , function () {
+   filter.classList.toggle("toggle_hide")
+  })
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+  var inform_probl_block=document.getElementsByClassName("inform_probl_block")[0];
+  var inform_probl_btn=document.getElementById("inform_probl_btn");
+  inform_probl_btn.addEventListener("click",function (ev){
+    inform_probl_block.classList.toggle("hide_probl_block")
+  })
 
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
+  var registr_block=document.getElementsByClassName("registr_block")[0]
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-
-
-  $scope.open = function($event) {
-    $scope.status.opened1 = true;
-  };
-  $scope.open2 = function($event) {
-    $scope.status.opened2 = true;
-  };
+  var registration_btn=document.getElementById("registration")
+  registration.addEventListener("click",function (){
+    
+    registr_block.classList.toggle("visibible")
+  })
 
 
-  $scope.setDate = function(year, month, day) {
-    $scope.dt = new Date(year, month, day);
-  };
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
+  $(document).add(".okay").click(function() {
+    $(".message").removeClass("active");
+  });
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
-
-  $scope.status = {
-    opened: false
-  };
-
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 2);
-  $scope.events =
-    [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
-
-  $scope.getDayClass = function(date, mode) {
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-      for (var i=0;i<$scope.events.length;i++){
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
-    }
-
-    return '';
-  };
-});
-app.controller("UserController",function ($scope, $http,$rootScope,$window){
-  $scope.user = {};
-    $scope.singinUser = function() {
-
-        $http({
-            method : 'POST',
-            url : '/login',
-            data : $scope.user
-        })
-        .then(function successCallback(data) {
-          $rootScope.userObj=data.data;
-          console.log( $rootScope.userObj);
-          $scope.user = {}
-
-        },
-        function errorCallback(data) {
-            console.log(data['status']);
-            $rootScope.error = true;
-         //$scope.error = "sorry "+ data['data']['login_status']
-            errorBlock.innerHTML = data['data']['login_status'];
-        })
-
-
-}
-});
-app.controller("RegistrCtrl",function ($scope, $http,$rootScope){
-  $scope.newUser = {};
-    $scope.singupUser = function() {
-      console.log($scope.newUser)
-        $http({
-            method : 'POST',
-            url : '/register',
-            data : $scope.newUser
-        })
-        .then(function successCallback(data) {
-          console.log(data.data)
-          $rootScope.userObj=$scope.newUser;
-
-          console.log( $rootScope.userObj)
-
-        },
-        function errorCallback(data) {
-          alert("sorry "+data.data.headers)
-            errorBlockRegister.innerHTML = 'user exist'
-        })
-}
-});
-app.controller("logOutUser",function ($scope,$window,$rootScope){
-  $scope.logOut=function (){
-
-    $rootScope.userObj= undefined;
-    $window.location="/logout"
-  }
-});

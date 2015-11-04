58068fa79de6790620c75d4bf8b6348403cdfee8
Vadim Padalko <vadime.padalko@gmail.com>
2015-11-04 14:43:02 +0200

    frontend

--- ecomap/db/ecomap/data/user.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` (first_name, last_name, email, password)
-VALUES ('admin', 'admin', 'admin.mail@gmail.com', '1111');
-INSERT INTO `user` (first_name, last_name, email, password)
-VALUES ('Анонім', 'Анонім', 'admin.mail@gmail.com', '1111');
--- ecomap/db/ecomap/schema/problem_type.sql
+++
@@ -1,9 +0,0 @@
-/*
-    This table provides names of all 
-    problem types.
-*/
-CREATE TABLE IF NOT EXISTS `problem_type` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `type` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;

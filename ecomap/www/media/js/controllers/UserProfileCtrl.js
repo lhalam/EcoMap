e687d213c3291de890a31cf828769879185f3c83
= <frut.jass779@gmail.com>
2015-12-01 22:12:51 +0200

    fixed errors during password change

--- ecomap/www/media/js/controllers/UserProfileCtrl.js
+++ ecomap/www/media/js/controllers/UserProfileCtrl.js
@@ -40,58 +40,28 @@ app.controller('UserProfileCtrl', ['$scope', '$state', '$cookies', '$http', 'toa
       new_pass: "",
       new_pass_confirm: ""
     };
-    // $scope.changePassword = function(passwd) {
-    //   console.log(passwd);
-    //   if(!passwd.old_pass || !passwd.new_pass || !passwd.new_pass_confirm){
-    //     return;
-    //   }
+    $scope.changePassword = function(passwd) {
+      if(!passwd.old_pass || !passwd.new_pass || !passwd.new_pass_confirm){
+        return;
+      }
 
-    //   var data = {};
-    //   data.id = $cookies.get('id');
-    //   data.old_pass = passwd.old_pass;
-    //   data.password = passwd.new_pass;
-    //   console.log(data);
-    //   $http({
-    //     method: 'POST',
-    //     url: '/api/change_password',
-    //     data: {
-    //       'id':data.id,
-    //       'old_pass':data.old_pass,
-    //       'password': data.new_pass
-    //     }
-    //   }).then(function successCallback(responce) {
-    //     passwd = {};
-    //     toaster.pop('success', 'Пароль', 'Пароль було успішно змінено!');
-    //     $scope.changePasswordForm.$setUntouched();
-    //   }, function errorCallback(responce) {
-    //     if (responce.status == 401) {
-    //       $scope.wrongOldPass = true;
-    //     }
-    //   });
-    // };
-
-    $scope.changePassword = function(){
-    var data = {};
-    data.id = $cookies.get('id');
-    data.old_pass = $scope.password.old_pass;
-    data.password = $scope.password.new_pass;
-    console.log(data);
-    $http({
+      var data = {};
+      data.id = $cookies.get('id');
+      $http({
         method: 'POST',
         url: '/api/change_password',
-        data: data
-    }).then(function successCallback(responce){
-        $scope.password = {};
+        data: {
+          'id': data.id,
+          'old_pass': $scope.password.old_pass,
+          'password': $scope.password.new_pass
+        }
+      }).then(function successCallback(responce) {
+        passwd = {};
         toaster.pop('success', 'Пароль', 'Пароль було успішно змінено!');
-        $scope.changePasswordForm.$setUntouched();
-    },
-      function errorCallback(responce){
-        if(responce.status == 401) {
+      }, function errorCallback(responce) {
+        if (responce.status == 401) {
           $scope.wrongOldPass = true;
-        };
-        if(responce.status == 400) {
-          toaster.pop('error', 'Пароль', 'Пароль не було змінено!')
-        };
+        }
       });
     };
 

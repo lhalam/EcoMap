c05061c7ac1990d3e49ed4074199b922bfc2dcff
= <frut.jass779@gmail.com>
2015-12-09 22:54:11 +0200

    edited controller to implement restore password

--- ecomap/www/media/js/controllers/RestorePasswordCtrl.js
+++ ecomap/www/media/js/controllers/RestorePasswordCtrl.js
@@ -1,15 +1,15 @@
-controller('RestorePasswordCtrl', ['$scope', '$state', '$http',
-  function($scope, $state, $http) {
+app.controller('RestorePasswordCtrl', ['$scope', '$state', '$http', '$location',
+  function($scope, $state, $http, $location) {
 
     $scope.restore = {};
     $scope.sendEmail = function(restore){
-        if(!restore.email){
+        if(!$scope.restore.email){
             return;
         }
 
         $http({
             method: 'POST',
-            url: '/api/url',
+            url: '/api/restore_password',
             data: $scope.restore
         }).then(function successCallback(response){
             //do what you need here
@@ -18,36 +18,42 @@ controller('RestorePasswordCtrl', ['$scope', '$state', '$http',
         })
     };
 
-    $scope.checkHashSum = function(){
-        $http({
-            method: 'GET',
-            url: '/api/url' + $state.params['hash_sum']
-        }).then(function successCallback(response){
-            //do what you need here
-        }, function errorCallback(){
-            //error callback if you need
-        })
-    }
+    // $scope.checkHashSum = function(){
+    //     $http({
+    //         method: 'GET',
+    //         url: '/api/url' + $state.params['hash_sum']
+    //     }).then(function successCallback(response){
+    //         //do what you need here
+    //     }, function errorCallback(){
+    //         //error callback if you need
+    //     })
+    // }
 
-    if($state.params['hash_sum']){
-        $scope.checkHashSum();        
-    }
+    // if($state.params['hash_sum']){
+    //     $scope.checkHashSum();        
+    // }
 
     $scope.newPass = {};
     $scope.updatePass = function(pass){
-        if(!pass.pass || !pass.confirmPass){
-            return;
-        }
+      hash_sum = window.location.href.split('/')[5];
+      if(!pass.pass || !pass.confirmPass){
+          return;
+      }
 
-        $http({
-            method: 'POST',
-            url: '/api/url',
-            data: pass
-        }).then(function successCallback(response){
-            //do what you need here
-        }, function errorCallback(){
-            //error callback if you need
-        })
+      $http({
+        method: 'PUT',
+        url: '/api/restore_password',
+        data: {
+          password: pass.pass,
+          confirm_pass: pass.confirmPass,
+          hash_sum: hash_sum.slice(0, -1)
+        }
+      })
+      .then(function successCallback(response){
+        window.location.href = 'http://134.249.141.35:81/#/login'
+      }, function errorCallback(){
+        //error callback if you need
+      })
     }
   }
 ]);

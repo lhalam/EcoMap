2a47b23a83710fb28c289dec9a9bc3bc7b0e3160
andjey <andjeypirat@gmail.com>
2015-12-23 21:01:11 +0200

    Made small changes to db

--- ecomap/db/ecomap/INSERT_DATA.sql
+++ ecomap/db/ecomap/INSERT_DATA.sql
@@ -3,3 +3,6 @@ SOURCE data/role.sql;
 SOURCE data/user_role.sql;
 SOURCE data/page.sql;
 SOURCE data/problem.sql;
+SOURCE data/resource.sql;
+SOURCE data/permission.sql;
+SOURCE data/role_permission.sql;

7500341c89eabc8a7d54bb83d5b2a5a54aef8518
Inna <innakutsik@gmail.com>
2016-01-18 17:44:38 +0200

    Revert "add fale config file and test v.1.2"
    This reverts commit 66d6d6a84ce39f8c7a4bffff362028c63578ed5d.

--- ecomap/unittest/src/python/ecomap/testInna.py
+++ ecomap/unittest/src/python/ecomap/testInna.py
@@ -1,8 +1,7 @@
 import os
 import unittest2
-from ConfigParser import SafeConfigParser
 
-import ecomap.config 
+import ecomap.config
 
 
 class TestConfig(unittest2.TestCase):
@@ -10,16 +9,13 @@ class TestConfig(unittest2.TestCase):
     	""" 
         Setting up for the test 
         """
-        self.config = ecomap.config.Config()
-        self.config.path = os.path.join(os.environ['PRODROOT'], 'unittest/data/ecomap.conf')
         self.parse_config = ecomap.config.Config().get_config() 
-        
  
     def tearDown(self):
     	"""
         Cleaning up after the test
         """
-        del self.config
+        del self.parse_config
  
     def test_return_dictionary(self):
     	return_type = type(self.parse_config)
@@ -29,22 +25,12 @@ class TestConfig(unittest2.TestCase):
         self.assertTrue(self.parse_config, "get_config() return empty dictionary")
 
     def test_wronge_extension(self):
-    	path = self.config.path .split(".")
+    	test_path = ecomap.config.CONFIG_PATH
+    	path = test_path.split(".")
     	self.assertEqual(path[1], "conf", "wronge extension of config file")
 
-    def test_wronge_file(self):
-        config_test = os.path.exists(self.config.path)
-        self.assertTrue(config_test, "there are no such file in this directory ")
-
-    def test_incorrect_config_file(self):
-        try:
-            config = SafeConfigParser()
-            config.readfp(open(self.config.path))
-            sections = config.sections()
-        except MissingSectionHeaderError or ParsingError:
-            print "The file does not exist, exiting gracefully"
-            self.assertTrue(False, "incorrect config file")
-
+ 
+ 
 if __name__ == '__main__':
     unittest2.main()
 

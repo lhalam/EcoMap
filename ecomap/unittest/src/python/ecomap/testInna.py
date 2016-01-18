48f1d841e6a0a95efcdefd4bd4504dc4e5eb1850
Inna <innakutsik@gmail.com>
2016-01-18 19:16:29 +0200

    test config v1.2

--- ecomap/unittest/src/python/ecomap/testInna.py
+++ ecomap/unittest/src/python/ecomap/testInna.py
@@ -1,7 +1,8 @@
 import os
 import unittest2
+from ConfigParser import SafeConfigParser
 
-import ecomap.config
+from ecomap.config import Config
 
 
 class TestConfig(unittest2.TestCase):
@@ -9,28 +10,57 @@ class TestConfig(unittest2.TestCase):
     	""" 
         Setting up for the test 
         """
-        self.parse_config = ecomap.config.Config().get_config() 
+        self.config = Config()
+        self.config.path = os.path.join(os.environ['PRODROOT'], 'unittest/data/ecomap.conf')
+        
  
     def tearDown(self):
     	"""
         Cleaning up after the test
         """
-        del self.parse_config
+        del self.config
  
     def test_return_dictionary(self):
-    	return_type = type(self.parse_config)
-        self.assertEqual(return_type, type({}), "get_config() return not dictionary type")
+        """ 
+        testing if config parser return a dictionary 
+        """
+    	return_type = type(self.config.get_config())
+        expected = dict
+        self.assertEqual(return_type, expected, "get_config() return not dictionary type")
 
     def test_return_not_empty_dictionary(self):
-        self.assertTrue(self.parse_config, "get_config() return empty dictionary")
+        """ 
+        testing if config parser return not empty dictionary
+        """
+        self.assertTrue(self.config.get_config(), "get_config() return empty dictionary")
 
     def test_wronge_extension(self):
-    	test_path = ecomap.config.CONFIG_PATH
-    	path = test_path.split(".")
-    	self.assertEqual(path[1], "conf", "wronge extension of config file")
+        """ 
+        testing if config file has extension conf  
+        """
+    	path = self.config.path.split(".")
+        expected = "conf"
+    	self.assertEqual(path[1], expected, "wronge extension of config file")
 
- 
- 
+    def test_wronge_file(self):
+        """ 
+        testing if config file is in directory 
+        """
+        config_test = os.path.exists(self.config.path)
+        self.assertTrue(config_test, "there are no such file in this directory ")
+
+    def test_incorrect_config_file(self):
+        """ 
+        testing if config file is correct 
+        """
+        try:
+            config = SafeConfigParser()
+            config.readfp(open(self.config.path))
+            sections = config.sections()
+        except MissingSectionHeaderError or ParsingError:
+            print "The file does not exist, exiting gracefully"
+            self.assertTrue(False, "incorrect config file")
+
 if __name__ == '__main__':
     unittest2.main()
 

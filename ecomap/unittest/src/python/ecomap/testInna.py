4a921403975f09fd0b3a19eec788a737ef8ac238
Inna <innakutsik@gmail.com>
2016-01-18 15:03:03 +0200

    test config.py v.1.1

--- ecomap/unittest/src/python/ecomap/testInna.py
+++ ecomap/unittest/src/python/ecomap/testInna.py
@@ -1,56 +1,33 @@
 import os
 import unittest2
+
 import ecomap.config
-from mock import Mock
 
 
 class TestConfig(unittest2.TestCase):
     def setUp(self):
-    	""" Setting up for the test """
-    	print "TestConfig:setUp: begin"
-
-    	self.return_dictionary = ecomap.config.Config().get_config() 
-
-    	testName = self.shortDescription()
-    	if testName == "Test routine is_return_dictionary":
-            print "setting up for test is_return_dictionary"
-        if testName == "Test routine is_return_not_empty_dictionary":
-            print "setting up for test is_return_not_empty_dictionary"
-        if testName == "Test routine wronge_path":
-            print "setting up for test wronge_path"
-
-        print "TestConfig:setUp: end"
+    	""" 
+        Setting up for the test 
+        """
+        self.parse_config = ecomap.config.Config().get_config() 
  
     def tearDown(self):
-    	"""Cleaning up after the test"""
-        print "TestConfig:tearDown: begin"
-
-    	testName = self.shortDescription()
-    	if testName == "Test routine is_return_dictionary":
-            print "cleaning up after test is_return_dictionary"
-        if testName == "Test routine is_return_not_empty_dictionary":
-            print "cleaning up after test is_return_not_empty_dictionary"
-        if testName == "Test routine wronge_path":
-            print "cleaning up after test wronge_path"
-
-        del self.return_dictionary
-
-        print "TestConfig:tearDown: end"
+    	"""
+        Cleaning up after the test
+        """
+        del self.parse_config
  
-    def test_is_return_dictionary(self):
-    	"""Test routine is_return_dictionary"""
-    	self.return_type = type(self.return_dictionary)
-        self.assertEqual(self.return_type, type({}), "get_config() return not dictionary type")
+    def test_return_dictionary(self):
+    	return_type = type(self.parse_config)
+        self.assertEqual(return_type, type({}), "get_config() return not dictionary type")
 
-    def test_is_return_not_empty_dictionary(self):
-    	"""Test routine is_return_not_empty_dictionary"""
-        self.assertTrue(self.return_dictionary, "get_config() return empty dictionary")
+    def test_return_not_empty_dictionary(self):
+        self.assertTrue(self.parse_config, "get_config() return empty dictionary")
 
-    def test_wronge_path(self):
-    	"""Test routine wronge_path"""
+    def test_wronge_extension(self):
     	test_path = ecomap.config.CONFIG_PATH
     	path = test_path.split(".")
-    	self.assertEqual(path[1], "conf", "wronge path to config file")
+    	self.assertEqual(path[1], "conf", "wronge extension of config file")
 
  
  

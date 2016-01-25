5cd174d7c6507b7245141a45ce4c225594a3b492
Oleksandr Stepanov <alexandrstepanov33@gmail.com>
2016-01-25 21:48:27 +0200

    test utils v1.1

--- ecomap/unittest/src/python/ecomap/test_utils.py
+++ ecomap/unittest/src/python/ecomap/test_utils.py
@@ -1,80 +1,70 @@
+"""Module which contains Test of Utils functions. """
+
+import re
 import unittest2
 
 from ecomap import utils
 
 
 #input data
-TEST_LINE = 20
-
-URL = "stackoverflow.com/questions/34908353/jquery-adding-removing-classes-on-a-button-to-hide-elements"
+PASSWORD_LENGTH = 20
+URL = "path/to/string/to/test"
 
 
 class TestUtils(unittest2.TestCase):
 
     """ Class for test utils.py"""
-     
+
     def setUp(self):
         """Setting up for the test."""
-        self.test_line = TEST_LINE
-        self.url = URL
 
     def tearDown(self):
         """Cleaning up after the test."""
 
-    def test_return_length_of_random_password(self):
-        """Testing if random_password returns a string sum of letters equals length."""
-        password = utils.random_password(self.test_line)
-        self.assertEqual(len(password),20,"random_password() return not valid password")
+    def test_length_of_random_password(self):
+        """Testing random_password for string length."""
+        password = utils.random_password(PASSWORD_LENGTH)
+        self.assertEqual(len(password), PASSWORD_LENGTH)
 
-    def test_check_password_for_uppercases_lowercases(self):
-        """Testing if random_password return a string with lowercases and uppercases."""
-        password = utils.random_password(self.test_line)
-        test_lower = any(c.islower() for c in password)
-        test_upper = any(c.isupper() for c in password)
-        self.assertTrue(test_lower, "Test failed:no lowercases")
-        self.assertTrue(test_upper, "Test failed:no uppercases")
+    def test_password_upper_lowercases(self):
+        """Testing random_password for lower-& uppercases."""
+        password = utils.random_password(PASSWORD_LENGTH)
+        lower = any(c.islower() for c in password)
+        upper = any(c.isupper() for c in password)
+        self.assertTrue(lower, "Test failed:no lowercases")
+        self.assertTrue(upper, "Test failed:no uppercases")
 
-    def test_if_instance_Singleton(self):
+    def test_if_instance_singleton(self):
         """Testing if instance is Singleton."""
-        self.instance_A = utils.Singleton(int)
-        self.instance_B = utils.Singleton(int)
-        self.assertTrue(self.instance_A)
-        self.assertTrue(self.instance_B)
-        self.assertIs(self.instance_A, self.instance_B)
+        class ChildFirst(utils.Singleton):
+            """child singleton """
+            pass
+        class ChildSecond(utils.Singleton):
+            """child singleton """
+            pass
+        first_instance = ChildFirst(type)
+        second_instance = ChildSecond(type)
+        self.assertIs(first_instance, second_instance)
 
-    def test_url_parse_with_agr_true_and_path_none(self):
+    def test_url_parse_with_agr_true(self):
         """Check function parse_url when get_arg true."""
-        arg = True
-        path = None
-        split_path = "jquery-adding-removing-classes-on-a-button-to-hide-elements" 
-        result_url = utils.parse_url(self.url,arg,path)
-        self.assertEqual(split_path,result_url)
+        split_path = "".join(re.findall(r"\w+$", URL))
+        self.assertEqual(utils.parse_url(URL, True, False), split_path)
 
-    def test_url_parse_with_path_true_and_arg_none(self):
+    def test_url_parse_with_path_true(self):
         """Check function parse_url when get_path true."""
-        arg = None
-        path = True
-        split_path = "stackoverflow.com/questions/34908353" 
-        result_url = utils.parse_url(self.url,arg,path)
-        self.assertEqual(split_path,result_url)
+        split_path = '/'.join(URL.split('/')[:-1])
+        self.assertEqual(utils.parse_url(URL, False, True), split_path)
 
-    def test_url_parse_with_agr_true_and_path_true(self):
-        """Check function parse_url when get_arg true and get_path true."""
-        arg = True
-        path = True
-        split_path = "jquery-adding-removing-classes-on-a-button-to-hide-elements" 
-        result_url = utils.parse_url(self.url,arg,path)
-        self.assertEqual(split_path,result_url)
+    def test_url_parse_args_true(self):
+        """Check function parse_url get_arg and get_path true."""
+        split_path = "".join(re.findall(r"\w+$", URL))
+        self.assertEqual(utils.parse_url(URL, True, True), split_path)
 
-    def test_url_parse_with_agr_none_and_path_none(self):
-        """Check function parse_url when get_arg none and get_path none."""
-        arg = None
-        path = None
-        split_path = "stackoverflow.com/questions/34908353/jquery-adding-removing-classes-on-a-button-to-hide-elements" 
-        result_url = utils.parse_url(self.url,arg,path)
-        self.assertEqual(split_path,result_url)
+    def test_url_parse_agrs_none(self):
+        """Check function parse_url get_arg and get_path none."""
+        self.assertEqual(utils.parse_url(URL, False, False), URL)
 
-    
+
 if __name__ == "__main__":
     unittest2.main()
-

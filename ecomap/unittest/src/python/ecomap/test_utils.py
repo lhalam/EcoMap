c21cd9f0f35f5f9f32f341c1248c09c6c9f4a8bb
Oleksandr Stepanov <alexandrstepanov33@gmail.com>
2016-01-27 23:20:40 +0200

    test utils v1.2

--- ecomap/unittest/src/python/ecomap/test_utils.py
+++ ecomap/unittest/src/python/ecomap/test_utils.py
@@ -1,9 +1,9 @@
 """Module which contains Test of Utils functions. """
 
-import re
 import unittest2
 
 from ecomap import utils
+from urlparse import urlparse
 
 
 #input data
@@ -31,34 +31,31 @@ class TestUtils(unittest2.TestCase):
         password = utils.random_password(PASSWORD_LENGTH)
         lower = any(c.islower() for c in password)
         upper = any(c.isupper() for c in password)
-        self.assertTrue(lower, "Test failed:no lowercases")
-        self.assertTrue(upper, "Test failed:no uppercases")
+        self.assertTrue(lower)
+        self.assertTrue(upper)
 
     def test_if_instance_singleton(self):
         """Testing if instance is Singleton."""
-        class ChildFirst(utils.Singleton):
-            """child singleton """
-            pass
-        class ChildSecond(utils.Singleton):
-            """child singleton """
-            pass
-        first_instance = ChildFirst(type)
-        second_instance = ChildSecond(type)
+        class SingleChild(object):
+            """metaclass singleton"""
+            __metaclass__ = utils.Singleton
+        first_instance = SingleChild()
+        second_instance = SingleChild()
         self.assertIs(first_instance, second_instance)
 
     def test_url_parse_with_agr_true(self):
         """Check function parse_url when get_arg true."""
-        split_path = "".join(re.findall(r"\w+$", URL))
+        split_path = urlparse(URL).path.split('/')[-1]
         self.assertEqual(utils.parse_url(URL, True, False), split_path)
 
     def test_url_parse_with_path_true(self):
         """Check function parse_url when get_path true."""
-        split_path = '/'.join(URL.split('/')[:-1])
+        split_path = '/'.join(urlparse(URL).path.split('/')[:-1])
         self.assertEqual(utils.parse_url(URL, False, True), split_path)
 
     def test_url_parse_args_true(self):
         """Check function parse_url get_arg and get_path true."""
-        split_path = "".join(re.findall(r"\w+$", URL))
+        split_path = urlparse(URL).path.split('/')[-1]
         self.assertEqual(utils.parse_url(URL, True, True), split_path)
 
     def test_url_parse_agrs_none(self):
@@ -68,3 +65,4 @@ class TestUtils(unittest2.TestCase):
 
 if __name__ == "__main__":
     unittest2.main()
+

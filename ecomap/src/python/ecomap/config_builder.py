c76ec57396d504d440d4e1bdb1766ff2ece5f5c3
adrian.yavorski <adrian.yavorski@gmail.com>
2016-02-03 15:31:14 +0200

    Config builder v1.0

--- ecomap/src/python/ecomap/config_builder.py
+++ ecomap/src/python/ecomap/config_builder.py
@@ -1,11 +1,17 @@
+"""
+Configuration builder module.
+Module creates config files from user input data.
+"""
 import os
 import re
 
 from ConfigParser import SafeConfigParser
 
 CONFIG_PATH = os.path.join(os.environ['CONFROOT'], '_configvars.conf')
+CONFIG_FILES = os.path.join(os.environ['CONFROOT'], '_configfiles.conf')
 
-def config_variables_parser():
+
+def configvars_parser():
     """
     Parse config variables file.
     Returns:
@@ -23,39 +29,68 @@ def config_variables_parser():
             template_config[section].append(value)
     return template_config
 
+
 def validate_reg_expression(reg_exp, value):
     """
     Regular expression validation.
+    Input: value to check, regular expression.
     Rerurns:
         True or False.
     """
     return bool(re.match(reg_exp, value))
 
-def validate_type(value, value_type):
-    # if value_type is 'int':
-    #     value=int(value)
-    pass
 
-def input_variables(confvar_dict):
+def input_user_data(confvar_dict):
     """
-    Function accepts dictionary with list of variables's values.
+    Function collects data from user input.
+    Input: data - dictionary,which contains list of variable's value.
     Returns:
             dictionary where keys are variables for templates configs.
     """
     user_dict = {}
     for key, value in confvar_dict.iteritems():
         while True:
-            user_dict[key] = raw_input('[%s] %s [default:%s]: ' % (key, value[0], value[1])) \
-            or value[1]
+            user_dict[key] = raw_input('[%s] %s [default:%s]: '
+                                       % (key, value[0], value[1])) or value[1]
             if user_dict[key]:
                 if not validate_reg_expression(value[3], user_dict[key]):
                     print 'Must be  %s' % value[3]
                     user_dict[key] = None
-                elif value[2] == 'int':
-                    user_dict[key]=int(user_dict[key])
-                    break
-                else: 
+                else:
                     break
     return user_dict
 
-input_variables(config_variables_parser())
+
+def create_config_files(user_input):
+    """
+    Function creates 4 configurations files.
+    Input: data - dictionary
+    """
+    with open(CONFIG_FILES, 'r+') as file_conf:
+        for line in file_conf:
+            template_name = line.split(', ')[0]
+            file_name = line.split(', ')[-1]
+            with open(template_name, 'r+') as temp:
+                content = temp.readlines()
+                for row in content:
+                    for key in user_input:
+                        to_replace = ('$%s' % key)
+                        if to_replace in row:
+                            place = content.index(row)
+                            content[place] = \
+                                row.replace(to_replace, user_input[key])
+                            row = content[place]
+            new_file_name = os.path.join(os.environ['CONFROOT'], file_name)
+            with open(new_file_name, 'w+') as to_write:
+                to_write.writelines(content)
+
+
+def main():
+    """
+    Function runs config builder.
+    """
+    input_data = input_user_data(configvars_parser())
+    create_config_files(input_data)
+
+if __name__ == '__main__':
+    exit(main())

0d6c276f00ed4721370bbb375d0dd09ac77868bd
Inna <innakutsik@gmail.com>
2016-02-05 16:51:19 +0200

    add logging

--- ecomap/src/python/ecomap/config_builder.py
+++ ecomap/src/python/ecomap/config_builder.py
@@ -5,30 +5,26 @@ Module creates config files from user input data.
 import os
 import re
 import sys
+import logging
 
+from optparse import OptionParser
 from ConfigParser import SafeConfigParser
 
 CONFIG_VARS = os.path.join(os.environ['CONFROOT'], '_configvars.conf')
 CONFIG_FILES = os.path.join(os.environ['CONFROOT'], '_configfiles.conf')
 
-CONFIG_TYPES = {'str' :   {'regex' : '.*',
-                           'eval' : '%s'
-                           },
-                'bool' :  {'regex' : '^(?:False|True)$',
-                           'eval' : 'eval("%s")'
-                           },
-                'int' :   {'regex' : r'^-?\d+$',
-                           'eval' : 'eval("%s")'
-                           },
-                'float' : {'regex' : r'^-?\d*\.?\d+$',
-                            'eval' : 'eval("%s")'
-                           },
-                'list' :  {'regex' : r'^\[.*\]$',
-                           'eval' : '%s'
-                           },
-                'dict' :  {'regex' : r'^\{.*\}$',
-                           'eval' : '%s'
-                           },
+CONFIG_TYPES = {'str': {'regex': '.*',
+                        'eval': '%s'},
+                'bool': {'regex': '^(?:False|True)$',
+                         'eval': 'eval(%s)'},
+                'int': {'regex': r'^-?\d+$',
+                        'eval': 'eval(%s)'},
+                'float': {'regex': r'^-?\d*\.?\d+$',
+                          'eval': 'eval(%s)'},
+                'list': {'regex': r'^\[.*\]$',
+                         'eval': '%s'},
+                'dict': {'regex': r'^\{.*\}$',
+                         'eval': '%s'}
                 }
 
 
@@ -41,11 +37,13 @@ def configvars_parser():
     config = SafeConfigParser()
     config.readfp(open(CONFIG_VARS))
     sections = config.sections()
+    logging.info("Parse _configvars.conf")
     template_config = {}
     for section in sections:
         template_config[section] = []
         for (key, value) in config.items(section):
-            template_config[section].append(value or None)
+            template_config[section].append(value or 'mail@mail.com')
+    logging.debug('Dictionary with list of variables was created')
     return template_config
 
 
@@ -59,7 +57,6 @@ def check_regex(reg_exp, value):
     return bool(re.match(reg_exp, value))
 
 
-
 def input_user_data(confvar_dict):
     """
     Function collects data from user input.
@@ -68,6 +65,7 @@ def input_user_data(confvar_dict):
             dictionary where keys are variables for templates configs.
     """
     user_dict = {}
+    logging.info('Start to input data')
     for key, value in confvar_dict.iteritems():
         while True:
             user_dict[key] = raw_input('[%s] %s [default:%s]: '
@@ -76,15 +74,16 @@ def input_user_data(confvar_dict):
                 type_value = CONFIG_TYPES[value[2]]
                 if not check_regex(type_value['regex'], user_dict[key]):
                     print 'Invalid data! Should be type %s.' % value[2]
-                #checking if email is valid. 
-                elif len(value)>3:
+                # checking if email is valid.
+                elif len(value) > 3:
                     if not check_regex(value[3], user_dict[key]):
                         print 'Invalid data! Example: mail@mail.com.'
                     else:
                         break
                 else:
                     user_dict[key] = type_value['eval'] % user_dict[key]
-                    break            
+                    break
+    logging.debug('Dictionary with user\'s input data was created')
     return user_dict
 
 
@@ -109,22 +108,34 @@ def create_config_files(user_input):
     Input: data - dictionary
     """
     file_conf = read_file(CONFIG_FILES, 'as_list')
+    logging.info('Creating config files')
     for line in file_conf:
         template_name = os.path.join(os.environ['CONFROOT'],
-                                     'templates/'+line.split(', ')[0])
+                                     'templates/' + line.split(', ')[0])
         content = read_file(template_name)
         for key, value in user_input.items():
             content = content.replace('$%s' % key, value)
+
         new_file_name = os.path.join(os.environ['CONFROOT'],
-                                     line.split(', ')[1])
+                                     line.split(', ')[1].strip())
         write_file(new_file_name, content)
+    logging.debug('Config files created')
 
 
 def main():
     """
     Function runs config builder.
     """
-    input_user_data(configvars_parser())
+    parser = OptionParser('usage: %prog [options]')
+    parser.add_option('-v', '--verbosity', action='store', dest='verbosity',
+                      type=int, default=1, help='Verbosity level [0-3]')
+    (options, args) = parser.parse_args()
+    if options.verbosity == 1:
+        log_level = logging.INFO
+    elif options.verbosity >= 2:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
+    create_config_files(input_user_data(configvars_parser()))
 
 if __name__ == '__main__':
     sys.exit(main())

274fab07b79fc3c31ce7a6f3613df610a01f827c
Roman <derkachr@gmail.com>
2016-02-10 23:19:43 +0200

    session in memcached

--- ecomap/src/python/ecomap/app.py
+++ ecomap/src/python/ecomap/app.py
@@ -5,16 +5,13 @@ import logging.config
 from datetime import timedelta
 
 from flask import Flask
-from flask.ext.triangle import Triangle
 from flask.ext.autodoc import Autodoc
+from flask.ext.triangle import Triangle
+from werkzeug.contrib.cache import MemcachedCache
+from flask.ext.session import Session, MemcachedSessionInterface
 from ecomap.config import Config
 
-# from flask import Flask, session
-from flask.ext.session import Session, MemcachedSessionInterface
-# from flask.ext.memcache_session import Session
-from werkzeug.contrib.cache import MemcachedCache
 
-cache = MemcachedCache(['127.0.0.1:11211'])
 
 
 _CONFIG = Config().get_config()
@@ -28,19 +25,11 @@ logging.config.fileConfig(os.path.join(os.environ['CONFROOT'], '_log.conf'))
 logger = logging.getLogger('flask_app')
 app.config['SECRET_KEY'] = 'a7c268ab01141868811c070274413ea3c588733241659fcb'
 app.config["REMEMBER_COOKIE_DURATION"] = timedelta(days=14)     # user time lib
-
-app.config["SESSION_TYPE"] = 'memcached'
-app.session_interface = MemcachedSessionInterface(cache,'session1:')
-
-
+app.config['SESSION_TYPE']='memcached'
 app.config['OAUTH_CREDENTIALS'] = {
     'facebook': {
         'id': _CONFIG['oauth.facebook_id'],
         'secret': _CONFIG['oauth.facebook_secret']
     }
 }
-
-# Session(app)
-# sess = Session()
-# sess.init_app(app)
-
+Session(app)

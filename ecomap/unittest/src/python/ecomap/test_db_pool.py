b4d9b96da72bbdc93fd429724cd1b3c0470b4bc9
Oleksandr Stepanov <alexandrstepanov33@gmail.com>
2016-02-15 22:52:49 +0200

    edit test_db_pool.py

--- ecomap/unittest/src/python/ecomap/test_db_pool.py
+++ ecomap/unittest/src/python/ecomap/test_db_pool.py
@@ -19,19 +19,19 @@ def conn_mock(user, host, port, passwd, db, charset, init_command):
 
 class TestCase(unittest2.TestCase):
 
-    """Class for test db_pool.py"""
+    """Class for test db_pool.py."""
 
     def setUp(self):
-        """Setting up for the test"""
+        """Setting up for the test."""
         self.conn_original = db_pool.MySQLdb.connect
         db_pool.MySQLdb.connect = conn_mock
 
     def tearDown(self):
-        """Cleaning up after the test"""
+        """Cleaning up after the test."""
         db_pool.MySQLdb.connect = self.conn_original
 
     def test_create_conn(self):
-        """Tests check if connection was created"""
+        """Tests check if connection was created."""
         self.assertEqual(TEST_INSTANCE._create_conn()['connection'],
                          CONN_TUPLE)
 

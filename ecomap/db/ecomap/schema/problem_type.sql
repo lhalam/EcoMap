58f0f014bcf7de9e2c3ad42bc5865def17da68d3
yshlyusar <g27thebest@ua.fm>
2016-02-16 14:50:36 +0200

    add methods to db utils

--- ecomap/db/ecomap/schema/problem_type.sql
+++ ecomap/db/ecomap/schema/problem_type.sql
@@ -3,7 +3,9 @@
     problem types.
 */
 CREATE TABLE IF NOT EXISTS `problem_type` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `type` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `picture` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `radius` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

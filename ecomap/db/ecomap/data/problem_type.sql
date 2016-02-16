5a6a5fd85e81a2716c84d8fed4136eb29dc028e7
yshlyusar <g27thebest@ua.fm>
2016-02-16 15:38:33 +0200

    add problem typa data

---
+++ ecomap/db/ecomap/data/problem_type.sql
@@ -0,0 +1,7 @@
+insert into problem_type (id, picture, name, radius) values (1, '1.png', 'Проблеми лісів', 10);
+insert into problem_type (id, picture, name, radius) values (2, '2.png', 'Сміттєзвалища', 10);
+insert into problem_type (id, picture, name, radius) values (3, '3.png', 'Незаконна забудова', 10);
+insert into problem_type (id, picture, name, radius) values (4, '4.png', 'Проблеми водойм', 10);
+insert into problem_type (id, picture, name, radius) values (5, '5.png', 'Загрози біорізноманіттю', 10);
+insert into problem_type (id, picture, name, radius) values (6, '6.png', 'Браконьєрство', 10);
+insert into problem_type (id, picture, name, radius) values (7, '7.png', 'Інші проблеми', 10);

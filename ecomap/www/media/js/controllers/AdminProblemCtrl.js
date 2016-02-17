9d885624754fcf2f135c7b7c3795620d43f19ed6
Inna <innakutsik@gmail.com>
2016-02-17 18:59:20 +0200

    add delete of problem_types

--- ecomap/www/media/js/controllers/AdminProblemCtrl.js
+++ ecomap/www/media/js/controllers/AdminProblemCtrl.js
@@ -1,6 +1,8 @@
-app.controller('ProblemCtrl', ['$scope', '$http', 'toaster', '$rootScope', 'msg',
-  function($scope, $http, toaster, $rootScope, msg) {
-
+app.controller('ProblemCtrl', ['$scope', '$http', 'toaster', 'msg', 'msgError',
+  function($scope, $http, toaster,  msg, msgError) {
+    
+    $scope.msg = msg;
+    $scope.msgError = msgError;
     $scope.addProblemTypeModal = false;
     $scope.showAddPpoblemTypeModal = function() {
       $scope.addProblemTypeModal = true;
@@ -13,6 +15,23 @@ app.controller('ProblemCtrl', ['$scope', '$http', 'toaster', '$rootScope', 'msg'
       $scope.problemType = problemObj;
       console.log(problemObj);
       $scope.editProblemTypeModal = true;
+    };
+
+    $scope.deleteProblemType = function(id) {
+      $http({
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        url: '/api/problem_type',
+        data: {
+          'problem_type_id': id
+        }
+      }).then(function successCallback(data) {
+        $scope.msg.deleteSuccess('типу проблеми');
+      }, function errorCallback(response) {
+        $scope.msg.deleteError('типу проблеми', $scope.msgError['alreadyBinded']);
+      })
     };
 
   }])

79674fe9a481d0f665b0a28d50ef45aa2cbef97f
Inna <innakutsik@gmail.com>
2016-02-17 22:07:43 +0200

    add put problems_type

--- ecomap/www/media/js/controllers/AdminProblemCtrl.js
+++ ecomap/www/media/js/controllers/AdminProblemCtrl.js
@@ -9,12 +9,41 @@ app.controller('ProblemCtrl', ['$scope', '$http', 'toaster', 'msg', 'msgError',
       $scope.problemType = {};
     };
 
-
-    $scope.editProblemTypeModal = false;
-    $scope.showEditPpoblemTypeModal = function(problemObj) {
-      $scope.problemType = problemObj;
-      console.log(problemObj);
+    
+    $scope.showEditProblemTypeModal= function(id, picture, name, radius) {
+      $scope.editProblemTypeObj = {
+        'id': id,
+        'picture': picture,
+        'name': name,
+        'radius': radius
+      };
+      console.log($scope.editProblemTypeObj);
       $scope.editProblemTypeModal = true;
+    }
+    $scope.editProblemSubmit = function(editProblemTypeObj){
+      if (!editProblemTypeObj.name || !editProblemTypeObj.radius) {
+        $scope.msg.editError('типу проблеми', $scope.msgError['incorectData']);
+        return;
+      }
+      console.log($scope.editProblemTypeObj);
+      $http({
+        method: 'PUT',
+        url: '/api/problem_type',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        data: {
+          'problem_type_id': $scope.editProblemTypeObj['id'],
+          'problem_type_picture': $scope.editProblemTypeObj['picture'],
+          'problem_type_name': $scope.editProblemTypeObj['name'],
+          'problem_type_radius': $scope.editProblemTypeObj['radius']
+        }
+      }).then(function successCallback(data) {
+        $scope.editProblemTypeModal = false;
+        $scope.msg.editSuccess('типу проблеми');
+      }, function errorCallback(response) {
+        $scope.msg.editError('типу проблеми', $scope.msgError['alreadyExist']);
+      })
     };
 
     $scope.deleteProblemType = function(id) {

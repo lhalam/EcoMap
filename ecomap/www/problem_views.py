270ab1b843ca048bfd87a73b0d725be054926459
Inna <innakutsik@gmail.com>
2016-02-17 17:56:32 +0200

    add backend for problems_user

--- ecomap/www/problem_views.py
+++ ecomap/www/problem_views.py
@@ -171,7 +171,7 @@ def post_problem():
         return response
 
 
-@app.route('/api/usersProblem/<int:user_id>', methods=['GET'])
+@app.route('/api/usersProblem', methods=['GET'])
 def get_user_problems(user_id):
     """This method retrieves all user's problem from db and shows it in user
     profile page on `my problems` tab.
@@ -195,8 +195,13 @@ def get_user_problems(user_id):
         :statuscode 200: no errors
 
     """
+    offset = request.args.get('offset') or 0
+    per_page = request.args.get('per_page') or 5
+    user_id = request.args.get('user_id')
+    count = db.count_user_problems(user_id)
     problems_list = []
-    problem_tuple = db.get_user_problems(user_id)
+    total_count = {}
+    problem_tuple = db.get_user_problems(user_id, offset, per_page)
     logger.info(problem_tuple)
     for problem in problem_tuple:
         problems_list.append({'id': problem[0],
@@ -208,6 +213,9 @@ def get_user_problems(user_id):
                               'date': problem[6] * 1000,
                               'severity': problem[8],
                               'is_enabled': problem[7]})
+    if count:
+        total_count = {'total_problem_count': count[0]}
+    print total_count
     return Response(json.dumps(problems_list), mimetype='application/json')
 
 

4a3eb213caf2bb0e122e54ee6d9267c3cbc8b658
adrian.yavorski <adrian.yavorski@gmail.com>
2016-02-19 16:50:05 +0200

    New error message added

--- ecomap/www/media/js/factories/msgError.js
+++ ecomap/www/media/js/factories/msgError.js
@@ -2,6 +2,7 @@ app.factory('msgError', function(toaster) {
   return msgError = {
     'alreadyExist': 'Дане ім’я вже зарезервоване.',
     'alreadyBinded': 'Так як дані вже прив’язані.',
-    'incorectData': 'Так як дані невірні.'
+    'incorectData': 'Так як дані невірні.',
+    'incorrectPhoto': ' Неправильний формат фото або таке фото вже існує.'
   }
 })

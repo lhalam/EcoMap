7531a4ae86a2f0d54b2fa85731a732bb53bab3bf
adrian.yavorski <adrian.yavorski@gmail.com>
2016-02-19 13:47:18 +0200

    Validation for problem type post problems added

--- ecomap/src/python/ecomap/validator.py
+++ ecomap/src/python/ecomap/validator.py
@@ -28,6 +28,9 @@ LENGTHS = {'email': [5, 100],
            'title': [2, 255],
            'content': [2, 255],
            'problem_type_id': [1, 255],
+           'problem_type_name': [1, 50],
+           'problem_type_radius': [1, 10],
+           'problem_type_picture': [1, 50],
            'user_id': [1, 255],
            'type': [1, 255],
            'latitude': [-90.0, 90.0],
@@ -633,6 +636,51 @@ def problem_post(data):
     return status
 
 
+def problem_type_post(data):
+    """Validates problem type post form. Checks: problem type's name,
+        radius, picture.
+       :params: data - json object
+       :return: dictionary with status keyname and error keys. By
+                default status is True, and error is empty.
+                If validation failed, status changes to False
+                and error keyname saves error ERROR_MSG
+    """
+    status = {'status': True, 'error': []}
+    keys = ['problem_type_name', 'problem_type_radius', 'problem_type_picture']
+
+    for keyname in keys:
+        if not has_key(data, keyname):
+            status['error'].append({keyname: ERROR_MSG['has_key'] % keyname})
+        elif not data[keyname]:
+            status['error'].append({keyname: ERROR_MSG['check_empty']
+                                    % keyname})
+        elif keyname is 'problem_type_name':
+            if not check_string(data[keyname]):
+                status['error'].append({keyname: ERROR_MSG['check_string']
+                                        % keyname})
+            elif not check_minimum_length(data[keyname], LENGTHS[keyname][0]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_minimum_length']
+                                        % keyname})
+            elif not check_maximum_length(data[keyname], LENGTHS[keyname][1]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_maximum_length']
+                                        % keyname})
+        elif keyname is 'problem_type_radius':
+            if not check_minimum_length(data[keyname], LENGTHS[keyname][0]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_minimum_length']
+                                        % keyname})
+            elif not check_maximum_length(data[keyname], LENGTHS[keyname][1]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_maximum_length']
+                                        % keyname})
+
+    if status['error']:
+        status['status'] = False
+
+    return status
+
 def has_key(dictionary, keyname):
     """Validator function, which checks if there is all needed keys json
        object.
@@ -769,3 +817,5 @@ def check_coordinates_length(value, length):
     if float(value) >= length[0] and float(value) <= length[1]:
         result = True
     return result
+
+

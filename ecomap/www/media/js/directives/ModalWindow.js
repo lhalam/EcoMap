ff364507f64eac9f525d01b8dd7b93a4e3ba9b10
anna-kuzii <anna_kuzij@ukr.net>
2016-02-23 01:41:01 +0200

    fix subscribtion table and add reload after close

--- ecomap/www/media/js/directives/ModalWindow.js
+++ ecomap/www/media/js/directives/ModalWindow.js
@@ -4,7 +4,7 @@ app.directive('modal', function() {
           '<div class="modal-dialog">' + 
             '<div class="modal-content">' + 
               '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<button type="button" ng-click="close()" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
                 '<h4 class="modal-title">{{ title }}</h4>' + 
               '</div>' + 
               '<div class="modal-body" ng-transclude></div>' + 

83a026614bee256bf0a6325b365a25ddbfcbbe10
anna-kuzii <anna_kuzij@ukr.net>
2016-02-22 14:32:52 +0200

    fix problem with bd

--- ecomap/db/ecomap/schema/subscription.sql
+++ ecomap/db/ecomap/schema/subscription.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `subscription` (
   `id` int(10) unsigned NOT NULl AUTO_INCREMENT,
   `problem_id` int(10) unsigned NOT NUll, # problem, wich was voted
   `user_id` int(10) unsigned NOT NULL ,   # user who has voted
-  `date_subscriptions` int(11) YES NULL ,
+  `date_subscriptions` int(11) NOT NULL ,
   `severity` enum('1', '2', '3', '4', '5') NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

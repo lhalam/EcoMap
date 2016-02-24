dbe38810388d967eb0ed301aa50d6297d5968842
Roman <derkachr@gmail.com>
2016-02-24 17:58:52 +0200

    update permission

--- ecomap/db/ecomap/data/resource.sql
+++ ecomap/db/ecomap/data/resource.sql
@@ -23,4 +23,4 @@ INSERT INTO `resource` VALUES (1,'/'),(8,'/api/addResource'),
 (38,'/api/problem_type'),
 (41,'/api/subscription_post'), (42,'/api/subscription_delete'),
 (43,'/api/usersSubscriptions/:idUser'),(44,'/api/change_nickname'),
-(45,'/api/problem_subcomments/:parent_id');
+(45,'/api/problem_subcomments/:idParent');

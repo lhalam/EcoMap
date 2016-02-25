9690652c8a03d904f319c53e6aa224e2f738c581
Roman <derkachr@gmail.com>
2016-02-25 12:30:28 +0200

    filter for answers

--- ecomap/www/media/js/factories/dictFilter.js
+++ ecomap/www/media/js/factories/dictFilter.js
@@ -5,3 +5,19 @@ app.filter('dictFilter', function (dict) {
     }
   };
 });
+
+app.filter('answerFilter',function (answer) {
+    return function(count){
+        if (count <5){
+            return count + ' ' + answer[count];
+        }
+        else if (count >= 5 && count <= 20){
+            return count + ' ' + answer['other'];
+        }
+        else {
+            count = String(count);
+            last_num = count.substr(count.length - 1);
+            return count + ' ' + (answer[last_num] || answer['other']);
+        }
+    };
+});

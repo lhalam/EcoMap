a98ae55b4fad2ec4d4181cf59b529262706b5754
Roman <derkachr@gmail.com>
2016-03-01 16:12:23 +0200

    update change nickname

--- ecomap/src/python/ecomap/validator.py
+++ ecomap/src/python/ecomap/validator.py
@@ -597,6 +597,40 @@ def change_password(data):
     return status
 
 
+def change_nickname(data):
+    """Validates change user nickname form. Checks old nickname,
+       new nickname and id of user.
+       :params: data - json object
+       :return: dictionary with status keyname and error keys. By
+                default status is True, and error is empty.
+                If validation failed, status changes to False
+                and error keyname saves error ERROR_MSG
+    """
+    status = {'status': True, 'error': []}
+    keyname = 'nickname'
+
+    if not has_key(data, keyname):
+        status['error'].append({keyname: ERROR_MSG['has_key'] % keyname})
+    elif not data[keyname]:
+        status['error'].append({keyname: ERROR_MSG['check_empty'] % keyname})
+    elif not check_string(data[keyname]):
+        status['error'].append({keyname: ERROR_MSG['check_string'] % keyname})
+    elif not check_minimum_length(data[keyname], LENGTHS[keyname][0]):
+        status['error'].append({keyname: ERROR_MSG['check_minimum_length']
+                                % keyname})
+    elif not check_maximum_length(data[keyname], LENGTHS[keyname][1]):
+        status['error'].append({keyname: ERROR_MSG['check_maximum_length']
+                                % keyname})
+    elif check_nickname_exist(data[keyname]):
+        status['error'].append({keyname:
+                                    ERROR_MSG['check_nickname_exist']}) 
+
+    if status['error']:
+        status['status'] = False
+
+    return status    
+
+
 def problem_post(data):
     """Validates problem post form.
        :params: data - json object

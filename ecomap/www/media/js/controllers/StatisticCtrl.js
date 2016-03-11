f62b95c8ba6a12c46e7d8fa57b1d90095d5f8c20
Inna <innakutsik@gmail.com>
2016-03-11 09:49:55 +0200

    add colors to statistic

--- ecomap/www/media/js/controllers/StatisticCtrl.js
+++ ecomap/www/media/js/controllers/StatisticCtrl.js
@@ -2,6 +2,9 @@ app.controller('StatisticCtrl', ['$scope', '$http', '$state', '$cookies', '$wind
   function($scope, $http, $state, $cookies, $window) {
     $scope.period = 0;
     $scope.chartObject = {};
+    $scope.chartObject.options = {
+        colors: ['rgb(9, 91, 15)', 'rgb(35, 31, 32)', 'rgb(152, 68, 43)', 'rgb(27, 154, 214)', 'rgb(113, 191, 68)', 'rgb(255, 171, 9)', 'rgb(80, 9, 91)']
+};
     $scope.chartObject.type = "PieChart";
     $scope.chartObject.data = {"cols": [
         {id: "t", label: "Topping", type: "string"},

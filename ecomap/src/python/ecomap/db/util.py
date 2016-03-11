75c9d6866da208b58954d8cc8be3410e4e5cd779
Inna <innakutsik@gmail.com>
2016-03-11 15:47:58 +0200

    add new function

--- ecomap/src/python/ecomap/db/util.py
+++ ecomap/src/python/ecomap/db/util.py
@@ -1152,9 +1152,8 @@ def get_all_users_problems(offset, per_page):
 
 @retry_query(tries=3, delay=1)
 def count_problems():
-    """
-
-    :return:
+    """Count all problems from db.
+    :return: count.
     """
     with pool_manager(READ_ONLY).manager() as conn:
         cursor = conn.cursor()
@@ -1166,7 +1165,7 @@ def count_problems():
 
 @retry_query(tries=3, delay=1)
 def count_user_problems(user_id):
-    """Count of user's problem
+    """Count of user's problem.
     :return: count
     """
     with pool_manager(READ_ONLY).manager() as conn:
@@ -1881,3 +1880,12 @@ def count_problem_types():
         cursor.execute(query)
         return cursor.fetchone()
 
+@retry_query(tries=3, delay=1)
+def count_photo():
+    """Count of all problem types
+    """
+    with pool_manager(READ_ONLY).manager() as conn:
+        cursor = conn.cursor()
+        query = """SELECT COUNT(photo.id) FROM `photo`;"""
+        cursor.execute(query)
+        return cursor.fetchone()

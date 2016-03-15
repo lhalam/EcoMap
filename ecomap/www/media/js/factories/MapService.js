0dd9d2c0ac548e6257961beb6b6458ddc8602ace
adrian.yavorski <adrian.yavorski@gmail.com>
2016-03-15 13:50:24 +0200

    Moderator logic added

--- ecomap/www/media/js/factories/MapService.js
+++ ecomap/www/media/js/factories/MapService.js
@@ -63,7 +63,10 @@ app.factory('MapFactory', ['$window', '$http', '$state', function(win, $http, $s
       method: 'GET',
       url: '/api/problems'
     }).then(function successCallback(response) {
+      console.log(response);
       angular.forEach(response.data, function (marker, key) {
+        if (marker.is_enabled == 0)
+          return;
         var pos = new google.maps.LatLng(marker.latitude, marker.longitude);
         var new_marker = new google.maps.Marker({
           position: pos,

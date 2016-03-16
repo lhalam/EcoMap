210d001243819cf24327b0793628b595a23756b2
yshlyusar <g27thebest@ua.fm>
2016-03-16 22:54:42 +0200

    add validation

--- ecomap/src/python/ecomap/validator.py
+++ ecomap/src/python/ecomap/validator.py
@@ -694,8 +694,52 @@ def problem_post(data):
     return status
 
 
+def problem_put(data):
+    """Validates problem edit form.
+       :params: data - json object
+       :return: dictionary with status keyname and error keys. By
+                default status is True, and error is empty.
+                If validation failed, status changes to False
+                and error keyname saves error ERROR_MSG
+    """
+    status = {'status': True, 'error': []}
+    keys = ['title', 'content', 'latitude', 'longitude', 'type']
+    for keyname in keys:
+        if not has_key(data, keyname):
+            status['error'].append({keyname: ERROR_MSG['has_key'] % keyname})
+        elif not data[keyname]:
+            status['error'].append({keyname:
+                                    ERROR_MSG['check_empty'] % keyname})
+        elif keyname in ['title', 'content']:
+            if not check_string(data[keyname]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_string'] % keyname})
+            elif not check_minimum_length(data[keyname], LENGTHS[keyname][0]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_minimum_length']
+                                        % keyname})
+            elif not check_maximum_length(data[keyname], LENGTHS[keyname][1]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_maximum_length']
+                                        % keyname})
+        elif keyname in ['latitude', 'longitude']:
+            if not check_coordinates(data[keyname]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_coordinates']
+                                        % data[keyname]})
+            if not check_coordinates_length(data[keyname], LENGTHS[keyname]):
+                status['error'].append({keyname:
+                                        ERROR_MSG['check_coordinates_length']
+                                        % data[keyname]})
+
+    if status['error']:
+        status['status'] = False
+
+    return status
+
+
 def problem_delete(data):
-    """Validates problem type delete form. Checks: id of problem type.
+    """Validates problem delete form. Checks: id of problem.
        :params: data - json object
        :return: dictionary with status keyname and error keys. By
                 default status is True, and error is empty.
@@ -704,6 +748,27 @@ def problem_delete(data):
     """
     status = {'status': True, 'error': []}
     keyname = 'problem_id'
+    if not has_key(data, keyname):
+        status['error'].append({keyname: ERROR_MSG['has_key'] % keyname})
+    elif not data[keyname]:
+        status['error'].append({keyname: ERROR_MSG['check_empty']
+                                % keyname})
+    if status['error']:
+        status['status'] = False
+    return status
+
+
+def problem_confirmation(data):
+    """Validates problem confirmation form. Checks: id of problem,
+        severity, status and is_enabled.
+       :params: data - json object
+       :return: dictionary with status keyname and error keys. By
+                default status is True, and error is empty.
+                If validation failed, status changes to False
+                and error keyname saves error ERROR_MSG
+    """
+    status = {'status': True, 'error': []}
+    keyname = ['problem_id', 'severity', 'status', 'is_enabled']
     if not has_key(data, keyname):
         status['error'].append({keyname: ERROR_MSG['has_key'] % keyname})
     elif not data[keyname]:

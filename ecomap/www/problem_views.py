6e1f04b0d8505fd1c4d8c06183acf49f3e6b6023
Oleksandr Stepanov <alexandrstepanov33@gmail.com>
2016-03-18 11:28:25 +0200

    add minor changes to problem_views.py

--- ecomap/www/problem_views.py
+++ ecomap/www/problem_views.py
@@ -55,7 +55,7 @@ def problems():
                 'problem_id': problem[0], 'title': problem[1],
                 'latitude': problem[2], 'longitude': problem[3],
                 'is_enabled': problem[4], 'problem_type_Id': problem[5],
-                'status': problem[6], 'date': problem[7], user_id: problem[8],
+                'status': problem[6], 'date': problem[7], 'user_id': problem[8],
                 'radius': problem[9], 'picture': problem[10]})
     return Response(json.dumps(parsed_json), mimetype='application/json')
 

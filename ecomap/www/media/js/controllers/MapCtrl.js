8d320c3fc10e4e657dbfc39d71b6e77515732899
Oleksandr Stepanov <alexandrstepanov33@gmail.com>
2016-03-28 21:47:01 +0300

    add sitemap links

--- ecomap/www/media/js/controllers/MapCtrl.js
+++ ecomap/www/media/js/controllers/MapCtrl.js
@@ -1,11 +1,52 @@
-app.controller('MapCtrl', ['$scope', '$http', '$rootScope', '$state', 'MapFactory',
-  function($scope, $http, $rootScope, $state, MapFactory) {
+app.controller('MapCtrl', ['$scope', '$http', '$rootScope', '$state', 'MapFactory','$auth',
+  function($scope, $http, $rootScope, $state, MapFactory, $auth) {
     MapFactory.initMap();
     MapFactory.turnResizeOn();
+    $scope.auth = $auth.isAuthenticated();
     $scope.markers = MapFactory.loadProblems();
+    $scope.siteMapTrigger = false;
+    $scope.toogleSiteMap = function() {
+      $scope.siteMapTrigger = !$scope.siteMapTrigger;
+      $scope.filterTrigger = false;
+
+    }
+    $scope.triggerStat = function() {
+      var url = '/#/statistic';
+      window.open(url, '_blank');
+    };
+    $scope.triggerLogin = function() {
+      var url = '/#/login';
+      window.open(url, '_blank');
+    };
+    $scope.triggerRegister = function() {
+      var url = '/#/register';
+      window.open(url, '_blank');
+    };
+    $scope.triggerAddProblem = function() {
+      var url = '/#/addProblem';
+      window.open(url, '_blank');
+    };
+    $scope.triggerUserProblem = function() {
+      var url = '/#/user_profile/problems';
+      window.open(url, '_blank');
+    };
+    $scope.triggerUserInfo = function() {
+      var url = '/#/user_profile/info';
+      window.open(url, '_blank');
+    };
+    $scope.triggerUserComments = function() {
+      var url = '/#/user_profile/comments';
+      window.open(url, '_blank');
+    };
+    $scope.triggerUserSubscriptions = function() {
+      var url = '/#/user_profile/subscriptions';
+      window.open(url, '_blank');
+    };
+
     $scope.filterTrigger = false;
     $scope.toogleFilter = function() {
       $scope.filterTrigger = !$scope.filterTrigger;
+      $scope.siteMapTrigger = false;
     }
 
      $scope.loadProblemType = function() {

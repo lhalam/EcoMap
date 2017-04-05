e2462916daef978062096a4a914c1a340f1e55dc
Anastasiia Bondarenko <anastasiabondarenko007@gmail.com>
2017-04-05 22:42:18 +0300

    Refactored

--- page_object_ecomap/tests/TestLoginAsAdmin.py
+++ page_object_ecomap/tests/TestLoginAsAdmin.py
@@ -12,20 +12,23 @@ class TestLoginAsAdmin(TestBase):
         cls.login_page = LoginPage(cls.driver)
         cls.home_user_page = HomeUserPage(cls.driver)
 
-    def test_log_in_as_admin(self):
+    def test_1_open_login_page(self):
         self.assertEqual(self.home_page.get_current_url(), self.home_page.get_expected_url())
         self.assertTrue(self.home_page.is_element_present(*HomePageLocator.LOG_IN))
         self.home_page.get_login_page()
 
+    def test_2_assert_login_elements_are_present(self):
         self.assertTrue(self.login_page.is_element_present(*LoginPageLocator.EMAIL))
         self.assertTrue(self.login_page.is_element_present(*LoginPageLocator.PASSWORD))
         self.assertTrue(self.login_page.is_element_present(*LoginPageLocator.SUBMIT))
 
+    def test_3_log_in_as_admin(self):
         self.login_page.type(self.test_data.get("email"), *LoginPageLocator.EMAIL)
         self.login_page.type(self.test_data.get("password"), *LoginPageLocator.PASSWORD)
         self.login_page.click(*LoginPageLocator.SUBMIT)
         self.login_page.wait_until_home_page_is_loaded()
 
+    def test_4_assert_login_success(self):
         self.assertEqual(self.home_page.get_current_url(), self.home_user_page.get_expected_url())
         self.assertTrue(self.home_page.is_element_present(*HomeUserPageLocator.LOGOUT_LINK))
         self.assertTrue(self.home_page.is_element_present(*HomeUserPageLocator.USER_PROFILE_LINK))

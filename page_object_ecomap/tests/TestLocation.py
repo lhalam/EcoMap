46b0f83511212e1e6dc12a0518dededb00b7f897
AnastasiiaL <analatysh@gmail.com>
2017-04-07 19:14:21 +0300

    Chaning in structure of test

--- page_object_ecomap/tests/TestLocation.py
+++ page_object_ecomap/tests/TestLocation.py
@@ -10,17 +10,19 @@ class testLocation(TestBase):
         super(testLocation, cls).setUpClass()
         cls.home_page = HomePage(cls.driver)
         cls.add_problem = AddProblemPage(cls.driver)
+        cls.login_page = LoginPage(cls.driver)
+        cls.home_page.get_login_page()
+        cls.home_user_page = cls.login_page.login(cls.test_data.get("email"), cls.test_data.get("password"))
 
-    def test_1_check_correctness_of_url(self):
-        # login
-        login_page = self.home_page.get_login_page()
-        login_page.login(self.test_data.get("email"), self.test_data.get("password"))
+    def test_check_location(self):
 
-        # go to page where we can check our coordinates
-        add_problem = login_page.click_on_add_problem()
+        # check if "Add problem" tab is present
+        self.assertTrue(self.home_user_page.get_current_url(), self.home_user_page.get_expected_url())
+        self.assertTrue(self.home_user_page.is_element_present(*NavigationLocator.ADD_PROBLEM))
+
+        # go to Add problem page
+        add_problem = self.home_user_page.click_on_add_problem()
         self.assertEqual(add_problem.get_current_url(), add_problem.get_expected_url())
-
-    def test_2_compare_coordinates(self):
 
         # get coordinates by application
         found_coordinates = self.add_problem.click_on_find_me()
@@ -31,14 +33,12 @@ class testLocation(TestBase):
         self.assertTrue(self.add_problem.check_location(found_coordinates, actual_coordinates),
                         self.add_problem.get_reason_of_fail())
 
-    def test_3_check_widget(self):
-
-        self.add_problem.click_on_find_me()
         self.assertTrue(self.add_problem.is_location_widget_present())
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
 

d535398d49187d19fb38fdfcf17f120a275a2bf9
DmytroRybin <dmytro.rybin@gmail.com>
2017-04-07 21:44:30 +0300

    add registration to change pass test

--- page_object_ecomap/tests/TestUserChangePassword.py
+++ page_object_ecomap/tests/TestUserChangePassword.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from page_object_ecomap.tests.TestBase import TestBase
 from page_object_ecomap.framework.Pages import *
 
@@ -8,17 +9,20 @@ class TestUserChangePassword(TestBase):
     @classmethod
     def setUpClass(cls):
         super(TestUserChangePassword, cls).setUpClass()
+        cls.home_page = HomePage(cls.driver)
         cls.user_page = HomeUserPage(cls.driver)
         cls.login_page = LoginPage(cls.driver)
         cls.user_profile_page = UserProfilePage(cls.driver)
+        registration_page = cls.home_page.get_registration_page()
+        registration_page.reg(cls.test_data.get("registration_email") % cls.generate_random_email(),
+                              cls.test_data.get("registration_name"),
+                              cls.test_data.get("registration_surname"),
+                              cls.test_data.get("registration_nickname") % cls.generate_random_email(),
+                              cls.test_data.get("change_password_old_pass"),
+                              cls.test_data.get("change_password_old_pass"))
 
 
     def test_user_change_pass(self):
-        self.home_page.get_login_page()
-
-        self.login_page.login(self.test_data.get("email_for_change_pass"),
-                              self.test_data.get("pass_for_change_pass"))
-
         self.user_profile_page = self.home_page.get_user_profile_page()
         self.user_profile_page.change_pwd(self.test_data.get("change_password_old_pass"),
                                      self.test_data.get("change_password_new_pass"),
@@ -26,6 +30,9 @@ class TestUserChangePassword(TestBase):
 
         self.assertTrue(self.user_profile_page.is_success_popup_present())
 
+    @staticmethod
+    def generate_random_email():
+        return str(random.randint(1, 100000))
 
 if __name__ == '__main__':
     unittest.main()

f1e9932f6d68f1dca4375b1c55632b65a44e1892
DmytroRybin <dmytro.rybin@gmail.com>
2017-04-07 20:02:03 +0300

    userChangePassword test done

--- page_object_ecomap/tests/TestUserChangePassword.py
+++ page_object_ecomap/tests/TestUserChangePassword.py
@@ -1,26 +1,30 @@
 import unittest
 from page_object_ecomap.tests.TestBase import TestBase
 from page_object_ecomap.framework.Pages import *
-from tests.TestLogIn import TestLogin
 
 
-class TestUserChangePassword(unittest.TestCase, TestBase):
+class TestUserChangePassword(TestBase):
 
     @classmethod
     def setUpClass(cls):
-        TestBase.setUpClass(cls)
+        super(TestUserChangePassword, cls).setUpClass()
+        cls.user_page = HomeUserPage(cls.driver)
+        cls.login_page = LoginPage(cls.driver)
+        cls.user_profile_page = UserProfilePage(cls.driver)
+
 
     def user_change_pass(self):
-        TestLogin.test_log_in(self)
-        user_profile_page = self.home_page.get_user_profile_page()
-        user_profile_page.change_pwd(self.test_data.get("change_password_old_pass"),
+        self.home_page.get_login_page()
+
+        self.login_page.login(self.test_data.get("email_for_change_pass"),
+                              self.test_data.get("pass_for_change_pass"))
+
+        self.user_profile_page = self.home_page.get_user_profile_page()
+        self.user_profile_page.change_pwd(self.test_data.get("change_password_old_pass"),
                                      self.test_data.get("change_password_new_pass"),
                                      self.test_data.get("change_password_new_pass_repeat"))
-        self.assertTrue(user_profile_page.is_success_popup_present())
 
-    @classmethod
-    def tearDownClass(cls):
-        TestBase.tearDownClass(cls)
+        self.assertTrue(self.user_profile_page.is_success_popup_present())
 
 
 if __name__ == '__main__':

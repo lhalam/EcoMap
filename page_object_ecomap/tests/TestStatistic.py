93ce960a2eef9cdfe1e15a355a2da13d74745d1c
s.vyshnevskyy <navianspirit@gmail.com>
2017-04-10 06:47:00 +0300

    add statistic page test

--- page_object_ecomap/tests/TestStatistic.py
+++ page_object_ecomap/tests/TestStatistic.py
@@ -1,8 +1,11 @@
 from page_object_ecomap.tests.TestBase import TestBase
 import unittest
-import selenium.webdriver.support.ui as ui
 from selenium import webdriver
 from page_object_ecomap.framework.Pages import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 
 class TestStatistic(TestBase):
@@ -17,12 +20,47 @@ class TestStatistic(TestBase):
 
     def test_1_assert_statistic_page_is_open(self):
         _d = self.browser
-        wait = ui.WebDriverWait(self.browser, 40)
+        wait = WebDriverWait(self.browser, 40)
         wait.until(lambda _d: _d.find_element_by_css_selector("ul.nav:nth-child(1) > li:nth-child(2) > a:nth-child(1)"))
         element = self.browser.find_element_by_css_selector("ul.nav:nth-child(1) > li:nth-child(2) > a:nth-child(1)")
         element.click()
         self.assertEqual(self.statistic_page.get_expected_url(), self.statistic_page.get_current_url())
 
+    def test_2_assert_valid_stat_in_all_top(self):
+        _d = self.browser
+        wait = WebDriverWait(self.browser, 100)
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, "//ul[1][contains(@class,'all-statistic')]/li[1][text() != '']")))
+        _problems = int(
+            self.browser.find_element_by_css_selector("ul.all-statistic:nth-child(1) > li:nth-child(1)").text)
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, "//ul[2][contains(@class,'all-statistic')]/li[1][text() != '']")))
+        _subscriptions = int(
+            self.browser.find_element_by_css_selector("ul.all-statistic:nth-child(2) > li:nth-child(1)").text)
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, "//ul[3][contains(@class,'all-statistic')]/li[1][text() != '']")))
+        _comments = int(
+            self.browser.find_element_by_css_selector("ul.all-statistic:nth-child(3) > li:nth-child(1)").text)
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, "//ul[4][contains(@class,'all-statistic')]/li[1][text() != '']")))
+        _photos = int(
+            self.browser.find_element_by_css_selector("ul.all-statistic:nth-child(4) > li:nth-child(1)").text)
+        wait.until(lambda _d: _d.find_elements_by_xpath("//ul[@ng-repeat = 'subscription in subscriptions']"))
+        elements_in_subscription = self.browser.find_elements_by_xpath("//ul[@ng-repeat = 'subscription in subscriptions']")
+        wait.until(lambda _d: _d.find_elements_by_xpath("//ul[@ng-repeat = 'severity in severities']"))
+        elements_in_severities = self.browser.find_elements_by_xpath("//ul[@ng-repeat = 'severity in severities']")
+        if _comments > 0:
+            wait.until(lambda _d: _d.find_elements_by_xpath("//ul[@ng-repeat = 'problemcomm in problCommStats']"))
+            elements_in_commented = self.browser.find_elements_by_xpath("//ul[@ng-repeat = 'problemcomm in problCommStats']")
+        else: elements_in_commented = []
+
+        self.assertTrue(_subscriptions == len(elements_in_subscription))
+        self.assertTrue((_problems == len(elements_in_severities)) if _problems <= 10 else
+                        (len(elements_in_severities) == 10))
+        self.assertTrue(((len(elements_in_commented) <= _comments)
+                         and (len(elements_in_commented) <= 10)))
+
+    
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()

3b8a91b0d50c5cc0141e1c558cba852108a776b2
Administrator <dmytro.minochkin@gmail.com>
2017-04-22 10:29:15 +0300

    minor fix

--- page_object_ecomap/framework/DriverWrapper.py
+++ page_object_ecomap/framework/DriverWrapper.py
@@ -32,7 +32,7 @@ class Driver:
 
     @staticmethod
     def add_driver_settings(driver):
-        driver.implicitly_wait(40)
-        driver.set_page_load_timeout(50)
+        driver.implicitly_wait(10)
+        driver.set_page_load_timeout(20)
         driver.set_window_size(1280, 1024)
         return driver
